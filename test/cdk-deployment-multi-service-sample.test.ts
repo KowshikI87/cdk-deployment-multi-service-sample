76b923d254c054f6eec887ce3239ab7699f0aa31
KowshikI87 <kowshikislam8@gmail.com>
2023-03-10 11:17:03 -0500

    Initial commit

---
+++ test/cdk-deployment-multi-service-sample.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkDeploymentMultiServiceSample from '../lib/cdk-deployment-multi-service-sample-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-deployment-multi-service-sample-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkDeploymentMultiServiceSample.CdkDeploymentMultiServiceSampleStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
